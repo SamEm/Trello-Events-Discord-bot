5e2b813501174e1981eb9102e952a1141fd71d80
Logiz <l.samemami@gmail.com>
2017-08-18 22:15:04 +0200

    Add files via upload

--- getReports.js
+++ getReports.js
@@ -27,28 +27,5 @@ function getAllReports(trello, thisBot, thisdb, listID) {
         if(--i) loopReports(bot, db, info, i);
       }, 3000);
     })(thisBot, thisdb, get, get.length -1);
-
-/*
-    for (let i = 1; i < get.length; i++) {
-      setTimeout(function () {
-        ++reportNumb;
-        let shortLink = get[i].shortLink;
-        db.get('SELECT id FROM cards ORDER BY id DESC LIMIT 1', function(getIDerr, dbGetID) {
-          if(!!getIDerr) console.log(getIDerr);
-          db.get('SELECT * FROM cards WHERE card = ?', [shortLink], function(error, dbGet) {
-            if(!!error) console.log(error);
-
-            if(!dbGet) {
-              let newDate = new Date();
-              let currentTime = dateFormat(newDate, 'MM:ss');
-//             bot.createMessage(config.postChannelID, )
-//             db.run('INSERT INTO cards (id, card, msgid) VALUES (?, ?, ?)', [reportNumb, shortLink, ])
-              console.log(currentTime + " " + reportNumb + " " + shortLink);
-            }
-          });
-        });
-      }, 3500);
-    }
-    */
   });
 }
